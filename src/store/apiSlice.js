7a44c1410a0e0ee6f599e5d56160e6988834bef7
A_Rahim <abdul.rahim@portqii.com>
2024-09-23 00:23:33 +0530

    api updated

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -4,7 +4,7 @@ import { API_KEY } from "../config";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: `http://api.weatherapi.com/v1`,
+    baseUrl: `https://api.weatherapi.com/v1`,
     prepareHeaders: (headers) => {
       headers.set("Content-Type", "application/json");
       return headers;
